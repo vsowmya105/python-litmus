b3db547350d34e07ad54e21e015e25c4f789d8d4
sowmyav10 <99954998+sowmyav10@users.noreply.github.com>
2022-06-12 21:44:59 +0530

    Update python_prog1.py

--- python_prog1.py
+++ python_prog1.py
@@ -1,17 +1,68 @@
-import requests
-import json
-api_key="7106a13a8eae3fb9f9c1f9b606fc816b"
-lat=33.44
-lon=-94.04
-base_url2='http://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily,minutely,alerts&appid={api_key}'
-#base_url = "http://api.openweathermap.org/data/3.0/onecall?"
-#complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-#url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={api_key}&units=metric'
-response=requests.get(base_url2)
-x=response.json()
-if response.status_code != "404":
-    y=x['current']
-    for p in y:
-        print(p,'----------->',y[p])
+import requests, json
+ 
+# Enter your API key here
+api_key = "6f8247b10127c8090841adf88d69450c"
+ 
+# base_url variable to store url
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+ 
+# Give city name
+city_name = input("Enter city name : ")
+ 
+# complete_url variable to store
+# complete url address
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+ 
+# get method of requests module
+# return response object
+response = requests.get(complete_url)
+ 
+# json method of response object
+# convert json format data into
+# python format data
+x = response.json()
+ 
+# Now x contains list of nested dictionaries
+# Check the value of "cod" key is equal to
+# "404", means city is found otherwise,
+# city is not found
+if x["cod"] != "404":
+    for p in x:
+        print(p)
+    # store the value of "main"
+    # key in variable y
+    y = x["main"]
+ 
+    # store the value corresponding
+    # to the "temp" key of y
+    current_temperature = y["temp"]
+ 
+    # store the value corresponding
+    # to the "pressure" key of y
+    current_pressure = y["pressure"]
+ 
+    # store the value corresponding
+    # to the "humidity" key of y
+    current_humidity = y["humidity"]
+ 
+    # store the value of "weather"
+    # key in variable z
+    z = x["weather"]
+ 
+    # store the value corresponding
+    # to the "description" key at
+    # the 0th index of z
+    weather_description = z[0]["description"]
+ 
+    # print following values
+    print(" Temperature (in kelvin unit) = " +
+                    str(current_temperature) +
+          "\n atmospheric pressure (in hPa unit) = " +
+                    str(current_pressure) +
+          "\n humidity (in percentage) = " +
+                    str(current_humidity) +
+          "\n description = " +
+                    str(weather_description))
+ 
 else:
-    print(" Not a valid input ")
+    print(" City Not Found ")
