c40d260baf03aa1100f9de7931449753ad78d5fd
sowmyav10 <99954998+sowmyav10@users.noreply.github.com>
2022-06-12 21:39:30 +0530

    Update python_prog1.py

--- python_prog1.py
+++ python_prog1.py
@@ -3,7 +3,7 @@ import json
 api_key="7106a13a8eae3fb9f9c1f9b606fc816b"
 lat=33.44
 lon=-94.04
-base_url2=f'http://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily,minutely,alerts&appid={api_key}'
+base_url2='http://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily,minutely,alerts&appid={api_key}'
 #base_url = "http://api.openweathermap.org/data/3.0/onecall?"
 #complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 #url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={api_key}&units=metric'
