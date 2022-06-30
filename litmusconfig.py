a4366e1f14ffbb67121dc964fd5d950c5009a4b5
sowmyav10 <99954998+sowmyav10@users.noreply.github.com>
2022-06-30 12:27:38 +0530

    Update litmusconfig.py

--- litmusconfig.py
+++ litmusconfig.py
@@ -1,9 +1,10 @@
 import os
+import json
 import io
 import requests
 url='https://raw.githubusercontent.com/sowmyav10/python/blob/python/project1/dev.json'
 page = requests.get(url)
-page = page.json()
+print(page.json())
 project = os.environ['project_name']
 envi = os.environ['environment_name']
 expt = os.environ['experiment']
